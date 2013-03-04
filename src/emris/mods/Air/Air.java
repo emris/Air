b0cfee0be71a7ab08072a94e7e937efab8964980
emris <emris@pc-net.at>
2013-03-04 19:07:31 +0100

    import fix

--- src/emris/mods/Air/Air.java
+++ src/emris/mods/Air/Air.java
@@ -1,15 +1,10 @@
 package emris.mods.Air;
 
-import net.minecraft.command.ICommandManager;
-import net.minecraft.command.ServerCommandManager;
-import net.minecraft.server.MinecraftServer;
-import net.minecraft.src.ModLoader;
 import cpw.mods.fml.common.Mod;
-import cpw.mods.fml.common.Mod.Init;
 import cpw.mods.fml.common.Mod.Instance;
-import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.Mod.ServerStarting;
+import cpw.mods.fml.common.event.FMLServerStartingEvent;
 import cpw.mods.fml.common.network.NetworkMod;
-import cpw.mods.fml.relauncher.Side;
 
 @Mod(modid="Air", name="Replace to Air", version="1.0")
 @NetworkMod(clientSideRequired=false, serverSideRequired=false)
