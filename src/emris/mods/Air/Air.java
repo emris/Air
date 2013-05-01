f633a10242d6168a7a9e5176b9c6b601162f00ed
emris <emris@pc-net.at>
2013-05-01 13:24:30 +0200

    license + mc 1.5.1 + forge 629 + version bump

--- src/emris/mods/Air/Air.java
+++ src/emris/mods/Air/Air.java
@@ -1,3 +1,20 @@
+/*
+ *  Copyright (C) 2013  emris
+ *  https://github.com/emris/Air
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>
+ */
 package emris.mods.Air;
 
 import cpw.mods.fml.common.Mod;
@@ -6,7 +23,7 @@ import cpw.mods.fml.common.Mod.ServerStarting;
 import cpw.mods.fml.common.event.FMLServerStartingEvent;
 import cpw.mods.fml.common.network.NetworkMod;
 
-@Mod(modid="Air", name="Replace to Air", version="1.0")
+@Mod(modid="Air", name="Replace to Air", version="1.1")
 @NetworkMod(clientSideRequired=false, serverSideRequired=false)
 public class Air {
 	
--- src/emris/mods/Air/CommandAir.java
+++ src/emris/mods/Air/CommandAir.java
@@ -1,3 +1,20 @@
+/*
+ *  Copyright (C) 2013  emris
+ *  https://github.com/emris/Air
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>
+ */
 package emris.mods.Air;
 
 import java.util.List;
@@ -36,7 +53,7 @@ public class CommandAir extends CommandBase {
 					World world = curPlayer.worldObj;
 					int bID = world.getBlockId(mop.blockX, mop.blockY, mop.blockZ);
 					if (bID > 0) {
-						world.setBlockWithNotify(mop.blockX, mop.blockY, mop.blockZ, 0);
+						world.setBlockToAir(mop.blockX, mop.blockY, mop.blockZ);
 					}
 				}
 			}
